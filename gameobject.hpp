ea6a4db09d866cfccf02a911ffa8838cd3874a55
jimbo644664 <james.arcus64@gmail.com>
2015-11-18 13:33:51 +0800

    Create gameobject.hpp

---
+++ gameobject.hpp
@@ -0,0 +1,18 @@
+#ifndef GAMEOBJECT_HPP
+#define GAMEOBJECT_HPP
+
+namespace easy
+{
+
+class GameObject
+{
+public:
+    virtual ~GameObject() {}
+
+protected:
+    GameObject() = default;
+};
+
+}
+
+#endif // GAMEOBJECT_HPP
